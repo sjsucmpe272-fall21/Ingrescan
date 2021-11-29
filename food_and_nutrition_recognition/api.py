52ecbc705a7502c399f8926e91af8d85bcda3308
harshapadidala <padidalaharshavardhan@gmail.com>
2021-11-29 11:55:13 -0800

    init

--- food_and_nutrition_recognition/api.py
+++ food_and_nutrition_recognition/api.py
@@ -1,6 +1,7 @@
 from fastai.vision.image import open_image
 from flask import Flask, request, jsonify, make_response
 from fastai.vision import *
+import pandas as pd
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import uuid
@@ -16,7 +17,7 @@ app = Flask(__name__)
 userName = os.getenv('mysql_user')
 password = os.getenv('mysql_pass')
 host = os.getenv('mysql_host')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{userName}:{password}@{host}/IngreScan'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{userName}:{password}@{host}:@3306/IngreScan'
 db_obj = SQLAlchemy(app)
 
 [food_rec_model_global, knn_nutrition_model_global, nutrition_data_df_global] = api_config_init()
@@ -24,11 +25,10 @@ db_obj = SQLAlchemy(app)
 
 @app.route('/user', methods=['GET'])
 def get_all_users():
-
     users = User.query.all()
     output = []
     for user in users:
-        user_data = {'public_id': user.public_id, 'name': user.name, 'password': user.password}
+        user_data = {'public_id': user.public_id, 'fname': user.u_fname, 'lname': user.u_lname, 'password': user.u_pwd}
         output.append(user_data)
 
     return jsonify({'users': output})
@@ -40,7 +40,7 @@ def get_user(public_id):
     if not user:
         return jsonify({'message': 'No user found!'})
 
-    user_data = {'public_id': user.public_id, 'name': user.name, 'password': user.password}
+    user_data = {'public_id': user.public_id, 'fname': user.u_fname, 'lname': user.u_lname, 'password': user.u_pwd}
     return jsonify({'user': user_data})
 
 
@@ -49,7 +49,9 @@ def register_user():
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'], method='sha256')
     public_id = str(uuid.uuid4())
-    new_user = User(public_id=public_id, name=data['name'], password=hashed_password)
+    new_user = User(public_u_id=public_id, u_fname=data['fname'], u_lname=data['lname'], u_phone=data['phone'], u_email=data['email'], u_pwd=hashed_password)
+    import pdb
+    pdb.set_trace()
     db_obj.session.add(new_user)
     db_obj.session.commit()
 
@@ -59,9 +61,9 @@ def register_user():
 @app.route('/login')
 def login():
     auth = request.authorization
-    if not auth or not auth.username or not auth.password:
+    if not auth or not auth.email or not auth.password:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Login required!'})
-    user = User.query.filter_by(name=auth.username).first()
+    user = User.query.filter_by(u_email=auth.email).first()
     if not user:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Login required!'})
     if check_password_hash(user.password, auth.password):
@@ -97,8 +99,8 @@ def upload():
         "energy_100g": food_description['energy_100g'],
         "recommended_food_items": recommended_food_items
     }
-    img = Img(img=image.read(), name=filename, mimetype=mimetype, userId = public_id)
-    db_obj.session.add(img)
+    userFoodData = User_Food_Data(public_u_id = public_id, image=image.read(), foodname=food_description['food_item'], mimetype=mimetype)
+    db_obj.session.add(userFoodData)
     db_obj.session.commit()
 
     return response
