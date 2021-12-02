552fa61c058c88ff16627b020009fd44ff936671
Ronak <ronak.p.btechi16@ahduni.edu.in>
2021-12-01 23:34:09 -0800

    Making flask app return responses

--- food_and_nutrition_recognition/app.py
+++ food_and_nutrition_recognition/app.py
@@ -31,10 +31,10 @@ def get_all_users():
         for user in users:
             u_temp = {'public_u_id': user.public_u_id, 'fname': user.u_fname, 'lname': user.u_lname, 'email': user.u_email}
             output.append(u_temp)
-        return jsonify({'users': output})
+        return make_response(jsonify({'users': output}), 200)
     except Exception as e:
         db_obj.session.rollback()
-        return jsonify({'error': e})
+        return make_response('Internal Server Error', 500, {'Error': str(e)})
 
 
 @app.route('/user/<public_u_id>', methods=['GET'])
@@ -44,13 +44,13 @@ def get_user(public_u_id):
         user = user_data.query.filter_by(public_u_id=public_u_id).first()
         user_dict = {}
         if not user:
-            return jsonify({'message': 'No user found!'})
+            return make_response('User not found', 404)
 
         user_dict = {'public_u_id': user.public_u_id, 'fname': user.u_fname, 'lname': user.u_lname, 'password': user.u_pwd}
-        return jsonify({'user': user_dict})
+        return make_response(jsonify({'user': user_dict}), 200)
     except Exception as e:
         db_obj.session.rollback()
-        return jsonify({'error': e})
+        return make_response('Internal Server Error', 500, {'Error': str(e)})
 
 
 @app.route('/signup', methods=['POST'])
@@ -64,10 +64,10 @@ def register_user():
                              u_email=data['email'], u_pwd=hashed_password)
         db_obj.session.add(new_user)
         db_obj.session.commit()
-        return jsonify({'message': 'New user created!', 'id': public_u_id})
+        return make_response(jsonify({'message': 'New user created!', 'id': public_u_id}), 200)
     except Exception as e:
         db_obj.session.rollback()
-        return jsonify({'error': e})
+        return make_response('Internal Server Error', 500, {'Error': str(e)})
 
 
 @app.route('/login', methods=['POST'])
@@ -75,103 +75,109 @@ def login():
     try:
         auth = request.authorization
         if not auth or not auth.username or not auth.password:
-            return make_response('Could not verify', 401, {'WWW-Authenticate': 'Login required!'})
+            return make_response('Fields can not be empty.', 401, {'WWW-Authenticate': 'Username or Password empty'})
         user = user_data.query.filter_by(u_email=auth.username).first()
         if not user:
-            return make_response('Could not verify', 401, {'WWW-Authenticate': 'Login required!'})
+            return make_response('Username not found. Please signup as a new user.', 401,
+                                 {'WWW-Authenticate': 'Wrong Username'})
         if check_password_hash(user.u_pwd, auth.password):
             return jsonify({'message': 'The user has been logged in!', 'id': user.public_u_id})
-        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Login required!'})
+        return make_response('Invalid Credentials', 401, {'WWW-Authenticate': 'Wrong Password'})
     except Exception as e:
         db_obj.session.rollback()
-        return jsonify({'error': e})
+        return make_response('Internal Server Error', 500, {'Error': str(e)})
 
 
 @app.route('/<uid>/imageUpload', methods=['POST'])
 def upload(uid):
     try:
+        if len(uid) == 0:
+            return make_response('Could not verify', 401, {'WWW-Authenticate': 'Login required!'})
         cfg['curr_user_id'] = uid
         cfg['curr_ts_epoch'] = str(int(time.time()))
         image = request.files['image']
         if not image:
-            return jsonify({'message': 'No image uploaded. Please upload an image!'}), 400
+            return make_response('No file found. Please upload an image file.', 406)
 
         ALLOWED_EXTENSIONS = set(eval(cfg['allowed_extensions']))
-        if image and allowed_file(image.filename, ALLOWED_EXTENSIONS):
-            cfg['base_dir_path'] = os.path.abspath(os.path.dirname(__file__))
-            image_file_name = secure_filename(image.filename)
-            image_path = cfg['image_path'].format(cfg['base_dir_path'], image_file_name)
-            s3_image_key = cfg['s3_image_key'].format(cfg['curr_user_id'], cfg['curr_ts_epoch'], image_file_name)
+        if not allowed_file(image.filename, ALLOWED_EXTENSIONS):
+            return make_response('Only ' + cfg['allowed_extensions'][1:-1] + ' files supported. '
+                                 + "'" + image.filename.split('.')[-1] + "'" + ' file not supported.', 415)
 
-            image.save(image_path)
-            mimetype = image.mimetype
-            if not image_file_name or not mimetype:
-                return 'Bad upload!', 400
+        cfg['base_dir_path'] = os.path.abspath(os.path.dirname(__file__))
+        image_file_name = secure_filename(image.filename)
+        image_path = cfg['image_path'].format(cfg['base_dir_path'], image_file_name)
+        s3_image_key = cfg['s3_image_key'].format(cfg['curr_user_id'], cfg['curr_ts_epoch'], image_file_name)
 
-            img = open_image(image)
-            predicted_food_item = food_predict(food_rec_model_global, img)
-            if len(predicted_food_item) == 0:
-                return jsonify({'message': 'Picture not clear. Please click clear picture of the food item.'})
+        image.save(image_path)
+        mimetype = image.mimetype
+        if not image_file_name or not mimetype:
+            return make_response('Bad file upload.', 400)
 
-            food_description = get_nutrition_info(nutrition_data_df_global, predicted_food_item)
-            if len(list(food_description.keys())) <= 1:
-                return jsonify({'message': 'Picture not clear. Please click clear picture of the food item.'})
+        img = open_image(image)
+        predicted_food_item = food_predict(food_rec_model_global, img)
+        if len(predicted_food_item) == 0:
+            return make_response('Picture not clear. Please click clear picture of the food item.', 406)
 
-            userFoodData = user_food_data(public_u_id=cfg['curr_user_id'],
-                                          image=os.path.join(cfg['s3'], cfg['bucket'], s3_image_key),
-                                          foodname=predicted_food_item, mimetype=mimetype,
-                                          timestamp=cfg['curr_ts_epoch'])
-            db_obj.session.add(userFoodData)
-            db_obj.session.commit()
+        food_description = get_nutrition_info(nutrition_data_df_global, predicted_food_item)
+        if len(list(food_description.keys())) <= 1:
+            return make_response('Picture not clear. Please click clear picture of the food item.', 406)
 
-            userNutritionData = user_nutrition_history(f_id=userFoodData.f_id,
-                                                       public_u_id=cfg['curr_user_id'],
-                                                       energy_100g=food_description['energy_100g'],
-                                                       carbohydrates_100g=food_description['carbohydrates_100g'],
-                                                       sugars_100g=food_description['sugars_100g'],
-                                                       proteins_100g=food_description['proteins_100g'],
-                                                       fat_100g=food_description['fat_100g'],
-                                                       fiber_100g=food_description['fiber_100g'],
-                                                       cholesterol_100g=food_description['cholesterol_100g'],
-                                                       timestamp=cfg['curr_ts_epoch'])
-            db_obj.session.add(userNutritionData)
-            db_obj.session.commit()
+        userFoodData = user_food_data(public_u_id=cfg['curr_user_id'],
+                                      image=os.path.join(cfg['s3'], cfg['bucket'], s3_image_key),
+                                      foodname=predicted_food_item, mimetype=mimetype,
+                                      timestamp=cfg['curr_ts_epoch'])
+        db_obj.session.add(userFoodData)
+        db_obj.session.commit()
 
-            with db_obj.get_engine().connect() as conn:
-                res = conn.execute("""SELECT SUM(`energy_100g`) AS energy,
-                SUM(`carbohydrates_100g`) as carbohydrates,
-                SUM(`proteins_100g`) as proteins,
-                SUM(`fat_100g`) as fat,
-                SUM(`fiber_100g`) as fiber,
-                SUM(`cholesterol_100g`) as cholesterol
-                FROM `IngreScan`.`user_nutrition_history`
-                WHERE UNIX_TIMESTAMP(FROM_UNIXTIME(`timestamp`)) > UNIX_TIMESTAMP(CURRENT_DATE()) 
-                AND UNIX_TIMESTAMP(FROM_UNIXTIME(`timestamp`)) < UNIX_TIMESTAMP(CURRENT_DATE() + INTERVAL 1 DAY);""")
+        userNutritionData = user_nutrition_history(f_id=userFoodData.f_id,
+                                                   public_u_id=cfg['curr_user_id'],
+                                                   energy_100g=food_description['energy_100g'],
+                                                   carbohydrates_100g=food_description['carbohydrates_100g'],
+                                                   sugars_100g=food_description['sugars_100g'],
+                                                   proteins_100g=food_description['proteins_100g'],
+                                                   fat_100g=food_description['fat_100g'],
+                                                   fiber_100g=food_description['fiber_100g'],
+                                                   cholesterol_100g=food_description['cholesterol_100g'],
+                                                   timestamp=cfg['curr_ts_epoch'])
+        db_obj.session.add(userNutritionData)
+        db_obj.session.commit()
 
-            user_history = []
-            for row in res.first():
-                user_history.append(row)
+        with db_obj.get_engine().connect() as conn:
+            res = conn.execute("""SELECT SUM(`energy_100g`) AS energy,
+            SUM(`carbohydrates_100g`) as carbohydrates,
+            SUM(`proteins_100g`) as proteins,
+            SUM(`fat_100g`) as fat,
+            SUM(`fiber_100g`) as fiber,
+            SUM(`cholesterol_100g`) as cholesterol
+            FROM `IngreScan`.`user_nutrition_history`
+            WHERE UNIX_TIMESTAMP(FROM_UNIXTIME(`timestamp`)) > UNIX_TIMESTAMP(CURRENT_DATE()) 
+            AND UNIX_TIMESTAMP(FROM_UNIXTIME(`timestamp`)) < UNIX_TIMESTAMP(CURRENT_DATE() + INTERVAL 1 DAY);""")
 
-            recommended_food_items = recommend_food(nutrition_data_df_global, knn_nutrition_model_global, user_history)
+        user_history = []
+        for row in res.first():
+            user_history.append(row)
 
-            response = {
-                "food": predicted_food_item,
-                "energy_100g": food_description['energy_100g'],
-                "carbohydrates_100g": food_description['carbohydrates_100g'],
-                "sugars_100g": food_description['sugars_100g'],
-                "proteins_100g": food_description['proteins_100g'],
-                "fat_100g": food_description['fat_100g'],
-                "fiber_100g": food_description['fiber_100g'],
-                "cholesterol_100g": food_description['cholesterol_100g'],
-                "recommended_food_items": recommended_food_items
-            }
+        recommended_food_items = recommend_food(nutrition_data_df_global, knn_nutrition_model_global, user_history)
 
-            s3_upload_data(cfg['bucket'], s3_image_key, image_path)
-            os.remove(image_path)
-            return response
+        response = {
+            "food": predicted_food_item,
+            "energy_100g": food_description['energy_100g'],
+            "carbohydrates_100g": food_description['carbohydrates_100g'],
+            "sugars_100g": food_description['sugars_100g'],
+            "proteins_100g": food_description['proteins_100g'],
+            "fat_100g": food_description['fat_100g'],
+            "fiber_100g": food_description['fiber_100g'],
+            "cholesterol_100g": food_description['cholesterol_100g'],
+            "recommended_food_items": recommended_food_items
+        }
+
+        s3_upload_data(cfg['bucket'], s3_image_key, image_path)
+        os.remove(image_path)
+        return make_response(response, 200)
     except Exception as e:
         db_obj.session.rollback()
-        return jsonify({'error': e})
+        return make_response('Internal Server Error', 500, {'Error': str(e)})
 
 
 if __name__ == "__main__":
